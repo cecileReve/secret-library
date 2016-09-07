2bc065af75ca06e5cfbeaa56540545d917411ddb
cecileReve <cilareve@gmail.com>
2016-09-07 15:24:39 +0200

    new

---
+++ secret-library/src/gestion/GestionDetailPanier.java
@@ -0,0 +1,80 @@
+package gestion;
+
+import classe.DetailPanier;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GestionDetailPanier {
+
+    private Connection connexion;
+
+    public GestionDetailPanier(Connection connexion) {
+        this.connexion = connexion;
+    }
+
+    public Connection getConnexion() {
+        return connexion;
+    }
+
+    public void setConnexion(Connection connexion) {
+        this.connexion = connexion;
+    }
+
+    public void sqlCreate(DetailPanier dp) {
+        try {
+            String query = "INSERT INTO detailPanier VALUES(?,?)";
+
+            PreparedStatement stmt = connexion.prepareStatement(query);
+            stmt.setInt(1, dp.getIdDetailPanier());
+            stmt.setInt(2, dp.getIdPanier());
+            stmt.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionDetailPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ResultSet sqlRead() {
+        ResultSet rs = null;
+        try {
+            String query = "SELECT * FROM detailPanier"
+                    + "JOIN panier ON panier.idPanier=detailPanier.idPanier";
+            Statement stmt = connexion.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionDetailPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+
+    public void sqlUpdate(DetailPanier dp, int idDetailPanier, int idPanier) {
+        try {
+            String query = "UPDATE detailPanier VALUES(?,?)WHERE idDetailPanier=" + idDetailPanier
+                    + "AND idPanier=" + idPanier;
+            PreparedStatement stmt = connexion.prepareStatement(query);
+            stmt.setInt(1, dp.getIdDetailPanier());
+            stmt.setInt(2, dp.getIdPanier());
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionDetailPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void sqlDelete(DetailPanier dp, int idDetailPanier, int idPanier) {
+
+        try {
+            String query = "DELETE FROM detailPanier WHERE idDetailPanier=" + idDetailPanier
+                    + "AND idPanier=" + idPanier;
+            Statement stmt = connexion.createStatement();
+            stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionDetailPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
---
+++ secret-library/src/gestion/GestionPanier.java
@@ -0,0 +1,78 @@
+package gestion;
+
+import classe.Panier;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GestionPanier {
+
+    private Connection connexion;
+
+    public GestionPanier(Connection connexion) {
+        this.connexion = connexion;
+    }
+
+    public Connection getConnexion() {
+        return connexion;
+    }
+
+    public void setConnexion(Connection connexion) {
+        this.connexion = connexion;
+    }
+
+    public void sqlCreate(Panier p) {
+        try {
+            String query = "INSERT INTO panier Values(?,?)";
+            PreparedStatement stmt = connexion.prepareStatement(query);
+            stmt.setInt(1, p.getIdClient());
+            stmt.setString(2, p.getNomPanier());
+            stmt.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public ResultSet sqlRead() {
+        ResultSet rs = null;
+        try {
+            String query = "SELECT * FROM panier"
+                    + "JOIN client ON client.idClient = panier.idPanier";
+            Statement stmt = connexion.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+
+    public void sqlUpdate(Panier p, int idPanier, int idClient) {
+        try {
+            String query = "UPDATE panier Values(?,?)WHERE idPanier=" + idPanier
+                    + "AND idClient=" + idClient;
+            PreparedStatement stmt = connexion.prepareStatement(query);
+            stmt.setInt(1, p.getIdClient());
+            stmt.setString(2, p.getNomPanier());
+            stmt.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void sqlDelete(Panier p, int idPanier, int idClient){
+        try {
+            String query = "DELETE FROM Panier WHERE idPanier=" + idPanier
+                    + "AND idClient=" + idClient;
+            Statement stmt = connexion.createStatement();
+            stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionPanier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
